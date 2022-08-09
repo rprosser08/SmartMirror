d02fdb2eb79e6e7027056bd377b6567d90b3913f
Richard Prosser <rdprosser@icloud.com>
2022-08-08 20:46:14 -0400

    Tkinter window opening in fullscreen mode

---
+++ MirrorMainFrame.py
@@ -0,0 +1,13 @@
+from tkinter import *
+from datetime import date
+
+class MirrorMainFrame:
+    def main():
+        window = Tk()
+        window.title("Smart Mirror")
+        window.attributes("-fullscreen", True)
+        window.mainloop()
+
+
+if __name__ == "__main__":
+    MirrorMainFrame.main()
