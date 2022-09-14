3f73ce021f4bfe408f8f7d0d199334a736c9ce6c
Richard Prosser <rdprosser@icloud.com>
2022-09-14 11:31:03 -0400

    Relevant weather data for next 5 hours nicely formatted into a dictionary for every hour

--- Weather.py
+++ Weather.py
@@ -1,4 +1,5 @@
 import requests, MirrorMainFrame, re, urllib.request, json
+from datetime import datetime
 
 
 class Weather:
@@ -13,6 +14,7 @@ class Weather:
     def getLocation():
         return MirrorMainFrame.MainFrame.getLocationData()
 
+    # Gets the location key from Accuweather needed to get the weather data for the location
     def getLocationKey():
         global apiKey, locationSet
         location = Weather.getLocation()
@@ -24,16 +26,53 @@ class Weather:
         locationSet = True
         return key
 
+    # Gets the 12 - hourly weather data for the location 
     def getHourlyWeather():
         global apiKey, key, locationSet
         Weather.setAPIKey()
         key = Weather.getLocationKey()
         URL = "http://dataservice.accuweather.com/forecasts/v1/hourly/12hour/" + key + "?apikey=" + apiKey + "&language=en-us&details=true&metric=false"
         response = requests.get(URL)
-        json = response.json()
-        print(json)
+        weatherData = response.json()
+        fiveHourData = Weather.formatData(weatherData)
+        print(fiveHourData)
 
+    # Gets the Accuweahter API key from the file "APIKey.txt"
     def setAPIKey():
         global apiKey
         with open("APIKey.txt") as f:
             apiKey = f.read()
+
+    # Stores the first 5 hours of weather data in the formatted data list as individal dictionaries for each hour
+    def formatData(weatherDataList):
+        formattedData = []
+
+        # Gets the frist 5 hours of weather data into the correct data structures
+        for i in range(5):
+            # Dictionary to store each data value (Time, Temp, Feel Like Temp, Precipitation Chance)
+            dataDictionary = {}
+
+            # Gets the time from the weather data and formats it to 12-hour clock -> 12:00 AM and stores it in the dictionary
+            weatherTime = weatherDataList[i]["DateTime"]
+            FullTime = weatherTime.split("T")[1]
+            _24HrTime = FullTime.split("-")[0]
+            time_object = datetime.strptime(_24HrTime, "%H:%M:%S").time()
+            time = time_object.strftime("%I:%M %p")
+            dataDictionary.update({"Time": time})
+
+            # Gets the temperature from the weather data and stores it in the dictionary
+            temp = weatherDataList[i]["Temperature"]["Value"]
+            dataDictionary.update({"Temp": temp})
+
+            # Gets the feels like temperature from the weather data and stores it in the dictionary
+            feelsTemp = weatherDataList[i]["RealFeelTemperature"]["Value"]
+            dataDictionary.update({"FeelsTemp": feelsTemp})
+
+            # Gets the chance of percipitation from the weather data and stores it in the dictionary
+            precipChance = weatherDataList[i]["PrecipitationProbability"]
+            dataDictionary.update({"PrecipChance": precipChance})
+
+            formattedData.append(dataDictionary)
+        
+        return formattedData
+
